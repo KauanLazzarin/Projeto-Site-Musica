0660f54ff1391c39e192daad92b682eb62488c30
KayLazz <kauanlazzarin@gmail.com>
2020-07-29 19:44:55 -0300

    ...

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require('express')
 const server = express()
 const port = 3003
-const nunJucks = require('nunjucks')
 
 server.use(express.static("public"))
 server.use(express.static("player-scripts"))
 
-nunJucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
-
 
 server.get('/', (req, res) => {
-    return res.render('index.html')
+    return res.sendFile(__dirname + '/src/views/index.html')
 })
 
 server.get('/player', (req, res) => {
-    return res.sendFile('C:/Users/Kauan/Desktop/KAUAN/projeto_site/player-scripts/player-page.html')
+    return res.sendFile(__dirname + '/player-scripts/player-page.html')
 })
 
 server.listen(port, () => {
