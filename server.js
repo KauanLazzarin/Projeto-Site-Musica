8c85bea3f85ebd32e229b16ca069c360974781ab
KayLazz <kauanlazzarin@gmail.com>
2020-08-01 11:37:32 -0300

    Página de playlist parcialmente adicionada

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ server.get('/player', (req, res) => {
     return res.sendFile(__dirname + '/player-scripts/player-page.html')
 })
 
+server.get('/userplaylists', (req, res) => {
+    return res.sendFile (__dirname + '/src/views/userPlaylists.html')
+})
+
 server.listen(port, () => {
     console.log(`Servidor rodando normalmente na porta ${port}`)
 })
